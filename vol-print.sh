1142958af20e430ef67ef8d42f5c9cea0ad8071a
lucoot <lucoot@gmail.com>
2018-10-09 13:31:36 -0700

    Update vol-print.sh

--- vol-print.sh
+++ vol-print.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 #prints music metadata out ttyAMA0 every second
 stty -F /dev/ttyAMA0 115200
 
 while true; do
         OUTPUT=$(volumio status)
-        echo -e  $OUTPUT | jq '{hi: .status, title: .title, artist: .artist, album: .album, seek: (.seek|tostring), duration: (.duration|tostring), samplerate: .samplerate, bitdepth: .bitdepth }'\
+        echo -e  $OUTPUT | jq '{status: .status, title: .title, artist: .artist, album: .album, seek: (.seek|tostring), duration: (.duration|tostring), samplerate: .samplerate, bitdepth: .bitdepth }'\
               > /dev/ttyAMA0
        sleep 1
 done
