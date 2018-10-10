ae61940b3737fad40b58a5782011ebef6233aa81
lucoot <lucoot@gmail.com>
2018-10-10 16:28:58 -0700

    Update vol-print.sh
    Fixed extraneous characters. 
    Added networking data.

--- vol-print.sh
+++ vol-print.sh
@@ -1,10 +1,15 @@
+
 #!/bin/bash
 #prints music metadata out ttyAMA0 every second
-stty -F /dev/ttyAMA0 115200
+stty -F /dev/ttyAMA0 11520
+
+IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print "\"ethernet\":\"" $1"\""}')
+WIFI=$( sudo iwconfig wlan0 | grep "Link Quality=" |cut -d= -f2 | cut -d/ -f1 | awk '{print "\"wifi\":\"" $1"\""}')
 
 while true; do
         OUTPUT=$(volumio status)
-        echo -e  $OUTPUT | jq '{status: .status, title: .title, artist: .artist, album: .album, seek: (.seek|tostring), duration: (.duration|tostring), samplerate: .samplerate, bitdepth: .bitdepth }'\
+        echo -e  $OUTPUT | jq -M  '{status: .status, title: .title, artist: .artist, album: .album, seek: (.seek|tostring), duration: (.duration|tostring), samplerate: .samplerate, bitdepth: .bitdepth,'$IP','$WIFI' }'\
               > /dev/ttyAMA0
-       sleep 1
+        sleep 1
 done
+
