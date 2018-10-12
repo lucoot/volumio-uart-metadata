e000c219d2897168423ce5098e6f25c59e50ec95
lucoot <lucoot@gmail.com>
2018-10-11 23:12:03 -0700

    Update vol-print.sh

--- vol-print.sh
+++ vol-print.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #prints music metadata out ttyAMA0 every second
 stty -F /dev/ttyAMA0 115200
@@ -7,7 +6,7 @@ while true; do
         OUTPUT=$(volumio status)
         IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print "\"" $1"\""}')
         WIFI=$(sudo iwconfig wlan0 | grep 'Link Quality=' |cut -d= -f2 | cut -d/ -f1 | awk '{print "\"" $1"\""}')
-
+        SYS="\"active\""
         if [[ -z $WIFI ]]
         then
             WIFI="\"0\""
@@ -17,7 +16,11 @@ while true; do
         then
             IP="\"0\""
         fi
-        echo -e  $OUTPUT | jq -M  '{status: .status, title: .title, artist: .artist, album: .album, seek: (.seek|tostring), duration: (.duration|tostring), samplerate: .samplerate, bitdepth: .bitdepth, ethernet: '$IP', wifi: '$WIFI'}'\
+        echo -e  $OUTPUT | jq -M  '{status: .status, title: .title, artist: .artist, album: .album, seek: (.seek|tostring), duration: (.duration|tostring), samplerate: .samplerate, bitdepth: .bitdepth, ethernet: '$IP', wifi: '$WIFI', system: '$SYS'}'\
               > /dev/ttyAMA0
         sleep 1
 done
+
+
+
+
